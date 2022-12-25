50cc585d255be3dda8b9a207e06dcef0d2f4c983
Tomer Figenblat <tomer@tomfi.info>
2022-12-25 15:27:33 +0200

    chore: added help for cli bin (#4)
    Signed-off-by: Tomer Figenblat <tomer@tomfi.info>
    
    Signed-off-by: Tomer Figenblat <tomer@tomfi.info>

--- src/cli.js
+++ src/cli.js
@@ -2,14 +2,64 @@
 
 const [,, ...args] = process.argv
 
-if (!('GITHUB_TOKEN' in process.env)) {
-  console.error('missing required environment variable GITHUB_TOKEN');
-  process.exit(1);
+function help() {
+  console.log(`
+    available options for github-viewer-stats:
+
+    contribs - prints user statistics
+    repo <repo-name> - prints repo statistics for <repo-name>
+    scopes - prints the requires scopes for GITHUB_TOKEN
+    help - prints this help
+    version - prints app version
+  `);
+}
+
+function scopes() {
+  console.log(`
+    required environment variable GITHUB_TOKEN scopes:
+
+    repo
+    read:packages
+    read:user
+    read:discussion
+  `);
+}
+
+function version() {
+  let pkgJsn = require('../package.json');
+  console.log(`${pkgJsn.name} ${pkgJsn.version}`);
+}
+
+function verifyToken() {
+  if (!('GITHUB_TOKEN' in process.env)) {
+    console.error('missing required environment variable GITHUB_TOKEN');
+    process.exit(1);
+  }
+}
+
+function output(out){
+  console.log(JSON.stringify(out, null, 2))
+}
+
+if ('help' == args[0]) {
+  help();
+  process.exit(0);
+}
+
+if ('version' == args[0]) {
+  version();
+  process.exit(0);
+}
+
+if ('scopes' == args[0]) {
+  scopes();
+  process.exit(0);
 }
 
 if (args.length == 0 || 'contribs' == args[0]) {
+  verifyToken();
   require('./contribs')()
-    .then(r => console.log(JSON.stringify(r, null, 2)))
+    .then(r => output(r))
     .then(() => process.exit(0));
 
 } else if ('repo' == args[0] || args[0].startsWith('repo=')) {
@@ -18,11 +68,13 @@ if (args.length == 0 || 'contribs' == args[0]) {
     console.error('repository name required')
     process.exit(1);
   }
+  verifyToken();
   require('./repo')(repo)
-    .then(r => console.log(JSON.stringify(r, null, 2)))
+    .then(r => output(r))
     .then(() => process.exit(0));
 
 } else {
-  console.error(`unclear request, ${args}`)
+  console.error(`unclear request, ${args}`);
+  help();
   process.exit(1);
 }
